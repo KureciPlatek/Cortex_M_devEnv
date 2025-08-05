55a55c790ac58621c95dc4a922520900facdea2d
Jeremie <jeremie.gallee@noser.com>
2025-08-05 09:52:26 +0200

    PRJ_RP2350 add blink and cyw43 hardware to project

--- prj_rp2350/app/main.c
+++ prj_rp2350/app/main.c
@@ -13,14 +13,24 @@
 #include "board.h"
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
 int main(void)
 {
    stdio_init_all();
+   if (cyw43_arch_init())
+   {
+      printf("CYW43 Wi-Fi chip init failed\n");
+      return -1;
+   }
+
    while (true)
    {
       printf("Hello, world!\n");
-      sleep_ms(1000);
+      cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+      sleep_ms(250);
+      cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+      sleep_ms(250);
    }
    return 0;
 }
