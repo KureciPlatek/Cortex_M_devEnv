2dbcb718d672d63e5e3efcdd7a797c81ca36b621
Jeremie <jeremie.gallee@noser.com>
2025-08-07 10:13:33 +0200

    FIX irq naming conflict between pico-sdk and ThreadX
    pico-sdk uses own names for irq vector table. They are all as .weak declared but
    are different from standard cmsis-core names and ThreadX's implemented handlers like
    PendSV_Handler or SysTick_Handler are not called.
    Add cmsis-core to target_link_libraries() so that rename_exceptions.h is called and
    ThreadX is happy

--- prj_rp2350/app/main.c
+++ prj_rp2350/app/main.c
@@ -1,13 +1,13 @@
 /**
  * @file    main.c
  * @author  Jeremie Gallee (jeremie.gallee@noser.com)
- * @brief   main file for RP2040 project example with ThreadX, 
- *          gdb/OpenOCD debugging and CMake project management 
+ * @brief   main file for RP2350 project example with ThreadX,
+ *          gdb/OpenOCD debugging and CMake project management
  * @version 0.1
  * @date    2025-07-22
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
 
 #include "board.h"
@@ -27,6 +27,11 @@ int main(void)
 
    while (true)
    {
+      printf("Hello, world!\n");
+//      cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+//      sleep_ms(250);
+//      cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+//      sleep_ms(250);
       /* Create, init and start ThreadX for this project */
       threadx_demo_start();
    }
